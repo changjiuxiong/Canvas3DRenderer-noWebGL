a4ba432c973084cf24eddbff8a3e6e6756283d3e
changjiuxiong <1119884256@qq.com>
2019-12-27 15:01:25 +0800

    feat: first commit

---
+++ demo/files.js
@@ -0,0 +1,12 @@
+var files = {
+    "demo": [
+        "demo_index",
+        "demo_rayCaster",
+        "demo_pbr",
+        "demo_wireframe",
+        "demo_effect",
+        "demo_gltf",
+        "demo_envMap",
+        "demo_textGeometry",
+    ]
+};
