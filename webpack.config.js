af4b0c9cddf63e21e29d089747ae2e02f6da4d91
changjiuxiong <31230281+changjiuxiong@users.noreply.github.com>
2020-06-02 14:37:30 +0800

    feat: 删掉多余的

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,6 @@ module.exports = {
     entry:  __dirname + "/src/main.js",//已多次提及的唯一入口文件
     output: {
         path: __dirname + "/dist",//打包后的文件存放的地方
-        filename: "bundle.js"//打包后输出文件的文件名
+        filename: "cool.js"//打包后输出文件的文件名
     }
 }
