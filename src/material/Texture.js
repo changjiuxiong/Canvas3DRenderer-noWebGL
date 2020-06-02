7aef36ae8e48fde7a7c7a5c0018a156d1be96ada
changjiuxiong <31230281+changjiuxiong@users.noreply.github.com>
2020-06-02 18:09:01 +0800

    feat: uv插值

--- src/material/Texture.js
+++ src/material/Texture.js
@@ -7,6 +7,22 @@ class Texture {
         this.wrapT = param.wrapT || COOL.REPEAT;
         this.magFilter = param.magFilter || COOL.NEAREST;
         this.minFilter = param.minFilter || COOL.NEAREST;
+
+        this.canvas = null;
+
+        this.initCanvas();
+    }
+
+    initCanvas(){
+        let that = this;
+        that.image.onload=function () {
+            let canvas = document.createElement('canvas');
+            canvas.width = 100;
+            canvas.height = 100;
+            let ctx = canvas.getContext("2d");
+            ctx.drawImage(that.image, 0, 0);
+            let imageData = ctx.getImageData(0, 0, canvas.width, canvas.height).data;
+        }
     }
 
     clone(){
